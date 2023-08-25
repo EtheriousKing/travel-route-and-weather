2e0e8e88cfdcae0a6972c95c94a1c4f4c1810687
Aaron Deril Dsouza <aaronddouza@gmail.com>
2023-08-25 13:58:58 +0530

    progress on route

--- app.js
+++ app.js
@@ -36,12 +36,21 @@ app.post("/getTravelWeather", async (req,res) => {
         const coordinates = await axios.get(`http://dev.virtualearth.net/REST/V1/Routes/Driving?wp.0=${req.body.currentLocation}%2Cwa&wp.1=${req.body.destination}%2Cwa&routeAttributes=routePath&key=${process.env.ROUTE_API_KEY}`); 
         const response = await axios.get(`https://dev.virtualearth.net/REST/v1/Imagery/Map/Road/Routes?wp.0=${req.body.currentLocation},WA;64;1&wp.1=${req.body.destination},WA;66;2&mmd=0&key=${process.env.ROUTE_API_KEY}`,{ responseType: 'arraybuffer' })
         const buffer64 = Buffer.from(response.data, 'binary').toString('base64');
-        console.log(coordinates.data.resourceSets[0].resources[0].routeLegs[0]);
-        res.render("index.ejs",{map : buffer64, location : coordinates.data.resourceSets[0].resources[0].routeLegs[0] });
+        res.render("index.ejs",{
+            map : buffer64, 
+            key : process.env.ROUTE_API_KEY,
+            startPoint : coordinates.data.resourceSets[0].resources[0].routeLegs[0].startLocation.name,
+            endPoint : coordinates.data.resourceSets[0].resources[0].routeLegs[0].endLocation.name,
+            startLat : coordinates.data.resourceSets[0].resources[0].routeLegs[0].actualStart.coordinates[0],
+            startLong : coordinates.data.resourceSets[0].resources[0].routeLegs[0].actualStart.coordinates[1],
+            endLat : coordinates.data.resourceSets[0].resources[0].routeLegs[0].actualEnd.coordinates[0],
+            endLong : coordinates.data.resourceSets[0].resources[0].routeLegs[0].actualEnd.coordinates[1]
+        });
     } catch (error) {
         console.error(error);
     }
 })
+
 
 app.listen(port, (res,req) => {
     console.log(`Server is listening on port: ${port}`);
